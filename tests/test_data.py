217bbe7b95a1ab00476aff637f33ad0609731c59
PC <skachat30913@gmail.com>
2024-08-21 01:41:51 +0400

    test_api_final

--- tests/test_data.py
+++ tests/test_data.py
@@ -28,32 +28,6 @@ TEST_DATA_CREATE = [
     }
 ]
 
-NEGATIVE_DATA_CREATE = [
-    {
-        "text": 12399,
-        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["password", "shrek"],
-        "info": {"False": ["True", "low"]}
-    },
-    {
-        "text": ["password", "shrek"],
-        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["password", "shrek"],
-        "info": {"url": ["info", "tags"]}
-    },
-    {
-        "text": (1, 3),
-        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["password", "shrek"],
-        "info": {"smol": ["red", "big"]}
-    },
-    {
-        "text": None,
-        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["password", "shrek"],
-        "info": {"extremal": ["relax", "sport"]}
-    }
-]
 
 TEST_DATA_UPDATE = {
     "id": 200,
@@ -63,39 +37,75 @@ TEST_DATA_UPDATE = {
     "info": {"0003333": ["1113333", "1111222"]}
 }
 
+NEGATIVE_DATA_CREATE = [
+    {
+        "text": 12399,
+        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
+        "tags": ["password", "dog"],
+        "info": {"False": [True, "low"]}
+    },
+    {
+        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
+        "tags": ["password", "cat"],
+        "info": {"url": [False, "tags"]}
+    },
+    {
+        "text": (1, 3),
+        "url": 1234,
+        "tags": ["password", "wolf"],
+        "info": {"smol": ["red", "big"]}
+    },
+    {
+        "text": None,
+        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
+        "tags": ["password", "scorpion"],
+        "info": {"extremal": ["relax", "sport"]}
+    },
+    {
+        "text": (1, 3),
+        "url": None,
+        "tags": ["password", "wolf"],
+        "info": {"smol": ["red", "big"]}
+    },
+    {
+        "text": (1, 3),
+    },
+]
+
 NEGATIVE_DATA_UPDATE = [
     {
-        "text": "Updated - How to enter a password",
+        "text": 1,
         "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["updated - password", "updated - shrek"],
+        "tags": ["updated - password", -0.5],
         "info": {"222222222": ["22222222222222", "3333333333333333"]}
     },
     {
         "id": {"000": ["111", "222"]},
         "text": " enter a password",
         "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["updated - password", "updated - shrek"],
+        "tags": ["updated - password", "updated"],
         "info": {"4444444444444": ["33333333333", "9999999999"]}
     },
     {
         "id": None,
         "text": "Updated  password",
-        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["updated - password", "updated - shrek"],
+        "url": "<scr<script>ipt>alert('1')</script>",
+        "tags": ["updated - password", '1000$'],
         "info": {"00000000000000": ["44444444444444444", "77777777777"]}
     },
     {
         "id": "string",
         "text": "Updated  password",
         "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["updated - password", "updated - shrek"],
+        "tags": ["updated - password", "golf"],
         "info": {"33333333": ["33333333333", "111111111111111"]}
     },
     {
         "id": 1,
         "text": 123,
-        "url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2010/07/01/16/405231.jpg",
-        "tags": ["updated - password", "updated - shrek"],
+        "url": "2' union SELECT concat(user_id, ':' ,first_name, ':' ,last_name),"
+               " concat(user, ':' ,password) from dvwa.users -- '",
+        "tags": ["updated - password", "golf"],
         "info": {"5555555555": ["555555555555", "55555555555555"]}
     }
 ]
