2c4b69d89b5d71b81262bedd305c67fb7dc8842c
PC <skachat30913@gmail.com>
2024-10-04 15:06:16 +0400

    !

--- endpoints/delete_meme.py
+++ endpoints/delete_meme.py
@@ -7,5 +7,5 @@ class DeleteMeme(Endpoint):
     @allure.step('удалить мем')
     def delete_meme_by_id(self, meme_id, token, headers=None):
         headers = headers if headers else self.get_headers(token)
-        self.response = requests.delete(f'{self.url}/{meme_id}', headers=headers)
+        self.response = requests.delete(f'{self}/{meme_id}', headers=headers)
         return self.response
